2b7bc3fde654984ab27e1e63c8971bd01c5f645d
SUJAY S <44267241+sujay12345@users.noreply.github.com>
2020-07-31 17:19:04 +0530

    Update views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from markdown2 import Markdown
 from django.urls import reverse
 from random import randint
-import random
+
 
 
 from . import util
